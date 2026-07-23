98fd330838461a326792d390ed2a4a41a7a460fa
Hillary Ilona <hillaryilona19@gmail.com>
2026-07-23 19:09:48 +0100

    refactor: format meta tags in _app.tsx for improved readability

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import type { AppProps } from 'next/app';
-import '@/app/globals.css';
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import "@/app/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+        <meta
+          name="facebook-domain-verification"
+          content="jrqej54duyox78e8o3kj5kt8wo9vzp"
+        />
       </Head>
       <Component {...pageProps} />
     </>
